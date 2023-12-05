4eecf21e5025f688b61b7b03315c2169bd0ae13a
YukiCR <chengrui0103@foxmail.com>
2023-12-05 20:19:27 +0800

    rm some files

--- SPART_based_modelling/runge_kutta_step.m
+++
@@ -1,8 +0,0 @@
-function next_state = runge_kutta_step(func, t, state, u, dt)
-    k1 = func(state, t, u);
-    k2 = func(state + 0.5 * dt * k1, t + 0.5 * dt, u);
-    k3 = func(state + 0.5 * dt * k2, t + 0.5 * dt, u);
-    k4 = func(state + dt * k3, t + dt, u);
-
-    next_state = state + (dt/6) * (k1 + 2*k2 + 2*k3 + k4);
-end
